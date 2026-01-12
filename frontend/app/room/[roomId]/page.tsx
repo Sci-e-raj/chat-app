2f7837810468fbc9a94fe5b975f5d2a5632e8978
Sci-e-raj <sairaj.mestry156@gmail.com>
2026-01-12 13:54:33 +0530

    added some css to room chat

--- frontend/app/room/[roomId]/page.tsx
+++ frontend/app/room/[roomId]/page.tsx
@@ -38,13 +38,10 @@ export default function RoomPage() {
         alert(data.message);
       }
     };
-
-    return () => {};
   }, [roomId, username]);
 
   function sendMessage() {
     const ws = getSocket();
-
     if (ws.readyState !== WebSocket.OPEN) return;
 
     ws.send(
@@ -57,15 +54,40 @@ export default function RoomPage() {
   }
 
   return (
-    <div>
-      <h1>Room {roomId}</h1>
+    <main className="min-h-screen bg-linear-to-br from-slate-900 to-black text-white flex flex-col">
+      {/* Header */}
+      <header className="p-4 border-b border-slate-700">
+        <h1 className="flex justify-center gap-1 text-xl font-semibold">
+          Room <span className="text-blue-400">{roomId}</span>
+        </h1>
+      </header>
 
-      {messages.map((m, i) => (
-        <div key={i}>{m}</div>
-      ))}
+      {/* Messages */}
+      <div className="flex-1 overflow-y-auto p-4 space-y-2">
+        {messages.map((m, i) => (
+          <div key={i} className="bg-slate-800 px-4 py-2 rounded-lg max-w-xl">
+            {m}
+          </div>
+        ))}
+      </div>
 
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={sendMessage}>Send</button>
-    </div>
+      {/* Input */}
+      <div className="p-4 border-t border-slate-700 flex gap-2">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type a message..."
+          className="flex-1 px-4 py-2 rounded-lg bg-slate-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+        />
+
+        <button
+          onClick={sendMessage}
+          className="px-6 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 transition font-semibold"
+        >
+          Send
+        </button>
+      </div>
+    </main>
   );
 }
